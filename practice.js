34a4cf56d5c0c5e7503b59f445750b9b623f54e3
Dominique <deejay.whitegrass@gmail.com>
2015-10-26 11:36:51 -0500

    third problem done

--- practice.js
+++ practice.js
@@ -62,9 +62,15 @@ that is named color, with the value being the color of your backpack. */
 
   //Code here
 
+var backPack = new Object();
+var item = "firstPocket";
+backPack[item] = "chapstick";
+
 //After you do the above, alert your entire backPack object.
 
   //Code here
+
+  alert(backPack);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
@@ -72,7 +78,7 @@ Instead, console.log your whole backPack object and then check out the console. 
 
   //Code here
 
-
+console.log(backPack);
 
 
 //NEXT PROBLEM
